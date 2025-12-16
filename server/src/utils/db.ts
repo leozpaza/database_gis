38b902b48603ab35841d55566c020043272e3532
leozpaza <152350430+leozpaza@users.noreply.github.com>
2025-12-16 20:47:04 +0300

    Validate env vars before starting server

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { env } from '../config/env.js';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: env.DATABASE_URL
+    }
+  },
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
 });
 
